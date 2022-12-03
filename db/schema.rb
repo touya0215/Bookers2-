079fefdf92d84d7b986d56e9e2b04a8e83792ea7
touya0215 <110541062+touya0215@users.noreply.github.com>
2022-12-03 15:03:10 +0000

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_27_113853) do
+ActiveRecord::Schema.define(version: 2022_12_03_101804) do
+
+  create_table "books", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
