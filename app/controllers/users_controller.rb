656ec5c29cffb74ec41adc240dbc9c3ae01d4123
touya0215 <110541062+touya0215@users.noreply.github.com>
2022-12-10 06:42:46 +0000

    add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  #get 'users/edit'
+  #get 'users/show'
+  #get 'users/index'
+  #上記を書き一行に変更
+  resources :users, only: [:show, :edit, :index, :update]
+  #get 'books/index'
+  #get 'books/show'
+  #get 'books/edit'
+  #上記を書き一行に変更
+  resources :users, only: [:show, :edit, :index]
   get 'homes/top'
   devise_for :users
   root to: 'homes#top'
