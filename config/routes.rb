cb8e2fdd8eab71b87314a8549fcafdd290d195ad
touya0215 <110541062+touya0215@users.noreply.github.com>
2022-12-11 06:22:11 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   #get 'users/edit'
   #get 'users/show'
   #get 'users/index'
@@ -8,9 +9,8 @@ Rails.application.routes.draw do
   #get 'books/show'
   #get 'books/edit'
   #上記を書き一行に変更
-  resources :users, only: [:show, :edit, :index]
+  resources :books, only: [:show, :edit, :index]
   get 'homes/top'
-  devise_for :users
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
